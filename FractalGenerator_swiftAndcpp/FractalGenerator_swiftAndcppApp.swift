8c0e47a184a4d8aa510f6be3ddb3865db4ecd4b5
Andrii_Mac <andrystarik@gmail.com>
2023-06-27 12:21:12 +0300

    Initial Commit

---
+++ FractalGenerator_swiftAndcpp/FractalGenerator_swiftAndcppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalGenerator_swiftAndcppApp.swift
+//  FractalGenerator_swiftAndcpp
+//
+//  Created by Andrey Starik on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FractalGenerator_swiftAndcppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
